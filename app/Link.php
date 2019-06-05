c8fed1dfaff8b3a5d4f8f43acde902bd3cda8450
vagrant <vagrant@homestead>
2019-06-05 15:38:54 +0000

    Specify an appends property on Link model.

--- app/Link.php
+++ app/Link.php
@@ -16,6 +16,10 @@ class Link extends Model
         'id',
     ];
 
+    protected $appends = [
+        'short'
+    ];
+
     public function getShortAttribute()
     {
         return $this->short();
