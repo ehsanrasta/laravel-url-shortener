810668e5e55d996495d8a4cbb7d30fa254ff12d6
eacdev <14826113+eacdev@users.noreply.github.com>
2019-11-30 15:29:47 -0500

    Implement small hover effect on status counters.

--- resources/js/components/dashboard/StatCounter.vue
+++ resources/js/components/dashboard/StatCounter.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="flex items-center flex-col w-full sm:w-40 md:w-48 lg:w-64 hover:shadow-lg bg-white rounded shadow pt-5 pb-10 mb-10">
+    <div class="flex grow items-center flex-col w-full sm:w-40 md:w-48 lg:w-64 hover:shadow-lg bg-white rounded shadow pt-5 pb-10 mb-10">
         <i :class="[icon, fgColor, bgColor]" class="fas text-2xl mb-2 p-4 rounded-full"></i>
         <h4 class="font-bold text-5xl">21.2k</h4>
         <p class="text-gray-600">Total Followers</p>
@@ -15,5 +15,6 @@
 </script>
 
 <style scoped>
-
+    .grow { transition: all .2s ease-in-out; }
+    .grow:hover { transform: scale(1.05); }
 </style>
