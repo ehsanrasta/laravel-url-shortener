1f2b2c24486747e235457b71f4632de7c4cba15d
eacdev <14826113+eacdev@users.noreply.github.com>
2019-12-02 16:11:46 -0500

    Show user data.

--- resources/js/components/layout/NavigationBar.vue
+++ resources/js/components/layout/NavigationBar.vue
@@ -1,20 +1,23 @@
 <template>
     <header class="bg-white shadow p-2 sm:flex sm:justify-between">
-        <div class="flex items-center justify-between">
+        <div class="flex items-baseline justify-between">
             <!-- todo: make this go somewhere -->
-            <a :href="APP_URL" class="text-purple-700 font-semibold subpixel-antialiased text-xl">Short.test</a>
+            <a :href="'/'" class="text-purple-700 font-semibold subpixel-antialiased text-xl">Short.test</a>
             <div class="sm:hidden">
                 <button @click="isOpen = !isOpen" type="button" class="block text-gray-500 hover:text-purple-700 focus:outline-none">
                     <svg class="h-6 w-6 fill-current" viewBox="0 0 24 24">
-                        <path v-if="isOpen" fill-rule="evenodd" d="M18.278 16.864a1 1 0 0 1-1.414 1.414l-4.829-4.828-4.828 4.828a1 1 0 0 1-1.414-1.414l4.828-4.829-4.828-4.828a1 1 0 0 1 1.414-1.414l4.829 4.828 4.828-4.828a1 1 0 1 1 1.414 1.414l-4.828 4.829 4.828 4.828z"/>
+                        <path v-if="isOpen" fill-rule="evenodd"
+                              d="M18.278 16.864a1 1 0 0 1-1.414 1.414l-4.829-4.828-4.828 4.828a1 1 0 0 1-1.414-1.414l4.828-4.829-4.828-4.828a1 1 0 0 1 1.414-1.414l4.829 4.828 4.828-4.828a1 1 0 1 1 1.414 1.414l-4.828 4.829 4.828 4.828z"/>
                         <path v-if="!isOpen" fill-rule="evenodd" d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z"/>
                     </svg>
                 </button>
             </div>
         </div>
-        <div :class="isOpen ? 'block' : 'hidden'" class="pt-2 pb-4 sm:flex">
-            <a :href="APP_URL + '/login'" class="block py-1 hover:text-purple-700 sm:py-0 sm:ml-5">Login</a>
-            <a :href="APP_URL + '/register'" class="block py-1 hover:text-purple-700 sm:py-0 sm:ml-5">Register</a>
+        <div :class="isOpen ? 'inline-block' : 'hidden'" class="pt-2 pb-4 sm:flex items-center">
+            <a v-if="!this.user" :href="'/login'" class="block py-1 hover:text-purple-700 sm:py-0 sm:ml-5">Login</a>
+            <a v-if="!this.user" :href="'/register'" class="block py-1 hover:text-purple-700 sm:py-0 sm:ml-5">Register</a>
+            <a v-if="this.user" :href="'/home'" class="block py-1 hover:text-purple-700 sm:py-0 sm:ml-5">Home</a>
+            <a v-if="this.user" :href="'/logout'" class="block py-1 hover:text-purple-700 sm:py-0 sm:ml-5">Logout</a>
         </div>
     </header>
 </template>
@@ -23,12 +26,14 @@
   export default {
     name: 'navigation-bar',
 
-    data() {
+    props: ['user'],
+
+    data () {
       return {
         APP_URL: process.env.MIX_APP_URL,
         isOpen: false,
       }
-    }
+    },
   }
 </script>
 
