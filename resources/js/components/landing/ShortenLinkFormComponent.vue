98d5afa906667b6a1bf3347d6ee4aac7c08b85f1
eacdev <14826113+eacdev@users.noreply.github.com>
2019-12-05 13:31:39 -0500

    Can now use enter key to shorten links.

--- resources/js/components/landing/ShortenLinkFormComponent.vue
+++ resources/js/components/landing/ShortenLinkFormComponent.vue
@@ -3,9 +3,11 @@
         <div class="flex mx-auto justify-between w-2/3 focus:outline-none border border-transparent focus-within:border-purple-500 rounded-full
                         shadow-md focus-within:shadow-lg">
             <input v-model="original" type="text" class="p-3 pl-5 w-10/12 z-10 text-purple-600 outline-none rounded-l-full"
+                   v-on:keyup.enter="shorten()"
                    placeholder="Paste URL">
             <button
                 @click="shorten()"
+                type="button"
                 class="w-6/12 bg-purple-500 hover:bg-purple-600 text-white font-bold py-3 px-4 z-0 -ml-5 rounded-full focus:outline-none">
                 <span class="ml-5 text-lg">Shorten</span>
             </button>
