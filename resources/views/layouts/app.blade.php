68def7597f03fbb815c6ba1e76142eb6ddd0b386
eacdev <14826113+eacdev@users.noreply.github.com>
2019-12-12 02:15:41 -0500

    It works

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -31,7 +31,7 @@
     </main>
 
     <div class="flex flex-row mt-20 mb-10 text-gray-600 justify-center">
-        <p>Test Webhooks! &copy; 2019</p>
+        <p>EACDEV &copy; 2019</p>
     </div>
 </div>
 </body>
