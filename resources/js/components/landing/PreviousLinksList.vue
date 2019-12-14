16363160862316ed8dba4cc16d07cff55f2f4e85
eacdev <14826113+eacdev@users.noreply.github.com>
2019-12-14 15:50:53 -0500

    Add border to seperate items, remove unused css.

--- resources/js/components/landing/PreviousLinksList.vue
+++ resources/js/components/landing/PreviousLinksList.vue
@@ -1,10 +1,10 @@
 <template>
-    <div class="mt-10 bg-white p-8 shadow w-full md:w-2/3 mx-auto">
+    <div class="rounded mt-10 bg-white p-8 shadow w-full md:w-2/3 mx-auto">
         <p class="uppercase font-semibold text-gray-600">Links</p>
-        <ul class="mt-8">
-            <li v-for="link in previousLinks" :key="link.id" class="flex flex-row mt-8">
-                <i class="fas fa-link text-xl bg-purple-100 text-purple-500 p-3 rounded self-center"></i>
-                <div class="flex flex-col ml-8">
+        <ul>
+            <li v-for="link in previousLinks" :key="link.id" class="flex flex-row mt-6 border-b border-gray-300">
+                <i class="fas fa-link text-xl bg-purple-100 text-purple-500 p-2 rounded self-center"></i>
+                <div class="flex flex-col ml-8  w-full pb-3">
                     <a :href="'/' + link.short" class="text-purple-600 font-semibold">{{ APP_URL }}/{{ link.short }}</a>
                     <a :href="link.original" class="text-purple-500 mt-1">{{ link.original | truncate(30) }}</a>
                 </div>
@@ -50,11 +50,4 @@
 </script>
 
 <style scoped>
-    .grow {
-        transition: all .2s ease-in-out;
-    }
-
-    .grow:hover {
-        transform: scale(1.05);
-    }
 </style>
