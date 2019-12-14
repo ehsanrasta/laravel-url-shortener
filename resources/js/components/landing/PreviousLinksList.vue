c67b30d005cedb1d88fb78913e65c9a27411ce41
eacdev <14826113+eacdev@users.noreply.github.com>
2019-12-14 16:17:40 -0500

    Fix alignment issue.

--- resources/js/components/landing/PreviousLinksList.vue
+++ resources/js/components/landing/PreviousLinksList.vue
@@ -2,11 +2,11 @@
     <div class="rounded mt-10 bg-white p-8 shadow w-full md:w-2/3 mx-auto">
         <p class="uppercase font-semibold text-gray-600">Links</p>
         <ul>
-            <li v-for="link in previousLinks" :key="link.id" class="flex flex-row mt-6 border-b border-gray-300">
+            <li v-for="link in previousLinks" :key="link.id" class="flex flex-row mt-6 border-b border-gray-300 items-center pb-3">
                 <i class="fas fa-link text-xl bg-purple-100 text-purple-500 p-2 rounded self-center"></i>
-                <div class="flex flex-col ml-8  w-full pb-3">
+                <div class="flex flex-col ml-8 w-full">
                     <a :href="'/' + link.short" class="text-purple-600 font-semibold">{{ APP_URL }}/{{ link.short }}</a>
-                    <a :href="link.original" class="text-purple-500 mt-1">{{ link.original | truncate(30) }}</a>
+                    <a :href="link.original" class="text-purple-500">{{ link.original | truncate(30) }}</a>
                 </div>
             </li>
         </ul>
